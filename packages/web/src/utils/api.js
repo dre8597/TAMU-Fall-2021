e95fcce4cee0d161514a034c4084464139fbdf7d
Sabrina Lu <sabrinalu999@tamu.edu>
2021-09-30 12:44:24 -0500

    print password and username in console instead of through alerts

--- packages/web/src/utils/api.js
+++ packages/web/src/utils/api.js
@@ -3,8 +3,8 @@ export const userLogin = async ({ username, password }) => {
     return new Promise((resolve, reject) => {
        
       setTimeout(() => {
-        alert(JSON.stringify(username) + " " + JSON.stringify(password));
-        
+        console.log(JSON.stringify(username) );
+        console.log(JSON.stringify(password) );
         if (username === 'sabrinalu999' && password === '12345678') {
             window.location.href = "dashboard";
           resolve();
